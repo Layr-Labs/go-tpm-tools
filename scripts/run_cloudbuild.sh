d39ca6304807b1e32296c5a01293a651480017f1
Solimander <85371573+solimander@users.noreply.github.com>
2026-03-03 15:40:33 -0700

    feat: add dev scripts for image builds and deployment (#14)
    * feat: add dev deployment script with PCR capture and Safe multisig proposal
    
    * bug fixes
    
    * add test pcrs to gitignore
    
    * fix url

---
+++ scripts/run_cloudbuild.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Run the script: ./scripts/run_cloudbuild.sh [debug|hardened|all]
+set -euxo pipefail
+
+BUILD_TYPE="${1:-hardened}"
+if [[ "$BUILD_TYPE" != "all" && "$BUILD_TYPE" != "debug" && "$BUILD_TYPE" != "hardened" ]]; then
+  echo "Usage: $0 [debug|hardened|all]"
+  exit 1
+fi
+
+# Append a timestamp, as there is a check in finish-image-build that checks if
+# the image already exists.
+IMAGE_SUFFIX="$USER-test-image-`date +%s`"
+PROJECT="${PROJECT:-tee-compute-sepolia-dev}"
+
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)
+echo "Running Cloud Build on directory $DIR (project: $PROJECT, build_type: $BUILD_TYPE)"
+
+# If you get the error:
+# googleapi: Error 403: Required 'compute.images.get' permission for 'foo', forbidden
+#
+# Ensure you grant Cloud Build access to Compute Images:
+# https://pantheon.corp.google.com/compute/images?referrer=search&tab=exports&project=$PROJECT_ID
+gcloud beta builds submit --config=${DIR}/launcher/cloudbuild.yaml \
+  --project="$PROJECT" \
+  --substitutions=_OUTPUT_IMAGE_SUFFIX="${IMAGE_SUFFIX}",_BUILD_TYPE="${BUILD_TYPE}"
+
+echo "Image creation successful."
+if [[ "$BUILD_TYPE" == "all" || "$BUILD_TYPE" == "debug" ]]; then
+  echo "Create a VM using the debug image eigen-compute-debug-${IMAGE_SUFFIX}"
+  echo "gcloud compute instances create confidential-space-test --image=eigen-compute-debug-${IMAGE_SUFFIX} --metadata ..."
+fi
+if [[ "$BUILD_TYPE" == "all" || "$BUILD_TYPE" == "hardened" ]]; then
+  echo "Or use the hardened image eigen-compute-hardened-${IMAGE_SUFFIX}"
+fi
