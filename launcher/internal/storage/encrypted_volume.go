34b888768159ddefa4804107c55f6a431558b657
Bowen Xue <93296844+bxue-l2@users.noreply.github.com>
2026-03-11 18:08:40 -0700

    differentiate error type (#20)

--- launcher/internal/storage/encrypted_volume.go
+++ launcher/internal/storage/encrypted_volume.go
@@ -133,6 +133,10 @@ func SetupSecondaryEncryptedVolume(logger logging.Logger, mnemonicProvider Mnemo
 func isLuksDevice(device string) (bool, error) {
 	cmd := exec.Command("cryptsetup", "isLuks", device)
 	if err := cmd.Run(); err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			// Non-zero exit code means it's not a LUKS device.
+			return false, nil
+		}
 		return false, err
 	}
 	return true, nil
